7d268ecfb0f77cc68a27fbd2d9a1044ce3e3c541
Zaid Rajput <zaid.rajput@gmail.com>
2023-06-25 13:45:04 +0500

    first commit

---
+++ Print pracitice.py
@@ -0,0 +1,15 @@
+print ("Hello world")
+a = ("hello world")
+print (a)
+print ("hello world", "how are you")
+print ("hello world", "how are you", sep="==")
+print ("hello world", end = ' ')
+print ("how are you")
+
+
+a = 5
+print("a =", a, sep='00000', end='\n\n\n')
+print("a =", a, sep='0', end='')
+
+
+
